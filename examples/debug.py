5575234abb7edf9a9b40786b91738c5b92cae0e4
Juan Nunez-Iglesias <jni@fastmail.com>
2024-03-18 10:45:07 +1100

    Clean up debug script

--- examples/debug.py
+++ examples/debug.py
@@ -1,28 +1,26 @@
 from platetrack.dock_widgets import _track_platelets
 import napari
-import numpy as np
 import zarr
+from pathlib import Path
 
+example_root = Path('/Users/abigailmcgovern/Data/iterseg/sample_data/annotrack_example/')
+
+sp = example_root / 'segmentations/230824_HxV3000is_plateseg2c.zarr'
+ip = example_root / 'images/201118_hTR4_DMSO_3000s_.zarr'
 
 
 napari_viewer = napari.Viewer()
 
-sp = '/Users/abigailmcgovern/Data/iterseg/sample_data/annotrack_example/segmentations/230824_HxV3000is_plateseg2c.zarr'
 s = zarr.open(sp)
 napari_viewer.add_labels(s, scale=(1, 2, .5, .5), name='seg')
 
-ip = '/Users/abigailmcgovern/Data/iterseg/sample_data/annotrack_example/images/201118_hTR4_DMSO_3000s_.zarr'
 ims = zarr.open(ip)
 for i in range(4):
     napari_viewer.add_image(ims[i, ...], name=f'img_{i}', scale=(1, 2, .5, .5))
 
 labels_layer = napari_viewer.layers['seg']
-
 image_layer = None
-
-
 use_all_image_layers = True
-
 sample_name = 'mishterman'
 treatment_name = 'nothingsir'
 x_microns, y_microns, z_microns = 0.5, 0.5, 2
